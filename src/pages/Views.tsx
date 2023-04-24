03d0a0f121f5f22f1984e158678b138423d082cf
Norbert Pawlenko <blinkz8@protonmail.com>
2023-04-24 20:14:29 +0200

    Add GenericList component and add it to Views page

--- src/pages/Views.tsx
+++ src/pages/Views.tsx
@@ -1,8 +1,22 @@
+import { Container, Typography, Box } from "@mui/material";
+import GenericList from "../components/GenericList";
+import { useAppSelector } from "../hooks/reduxHooks";
+import UserBox from "../components/UserBox";
+
 const Views = () => {
+    const users = useAppSelector((state) => state.users);
+
     return ( 
-        <>
-        Views
-        </>
+        <Container sx={{my: 4}}>
+            <GenericList
+                data={users}
+                keyExtractor={(user) => user?.id as number}
+                renderItem={(user) => (
+                    <UserBox {...user} />
+                )}
+
+            />
+        </Container>
      );
 }
  
