39f27c32db3d7ef834c2e7b21131445373a75588
Norbert Pawlenko <blinkz8@protonmail.com>
2023-04-24 20:27:42 +0200

    Update error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,18 @@
+import { Box, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import "../assets/ErrorPage.scss";
+
 const ErrorPage = () => {
+    const { t } = useTranslation();
+
     return ( 
-        <>
-        Error
-        </>
+        <Box
+            className="error-404"
+            maxWidth="sm"
+        >
+            <Typography variant="h2">{t('error.404')}</Typography>
+            <Typography>{t('pageNotFound')}</Typography>
+        </Box>
      );
 }
  
